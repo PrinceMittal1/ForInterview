5ee7ed54f24de2fe4ca95b1dbe72022564fe5cff
prince <princemittal345@gmai.com>
2023-03-21 16:07:05 +0530

    just created

---
+++ MyApp/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(){
+  
+  return (
+    <SafeAreaView >
+      <View><Text>helo</Text></View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
